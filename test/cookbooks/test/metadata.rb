243dc2903fdaadff342fdf63a17b96fe231b8829
Sukant-Yadav <Sukant.Yadav@johnmuirhealth.com>
2025-06-05 18:03:46 +0530

    pushing to new branch

---
+++ test/cookbooks/test/metadata.rb
@@ -0,0 +1,7 @@
+name 'test'
+version '0.0.1'
+
+depends 'apparmor'
+depends 'chef-mysql-sous-chefs'
+depends 'selinux'
+depends 'yum-mysql-community'
---
+++ test/cookbooks/test/recipes/installation_client.rb
@@ -0,0 +1,6 @@
+apt_update 'update'
+
+mysql_client_installation_package 'default' do
+  version node['mysql_test']['version']
+  action :create
+end
